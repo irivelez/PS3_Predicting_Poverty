3e6279219f02738344c6d007c3b183fb62be9978
Irina Velez Lopez <ia.velez61@uniandes.edu.co>
2023-07-26 15:34:03 -0500

    Deleting previous scripts

---
+++ scripts/1. Loading data.R
@@ -0,0 +1,20 @@
+############################################################
+#         Problem Set 3. Predicting Poverty
+#           Big data and Machine Learning
+#             Universidad de los Andes
+##########################################################
+
+#Authors:
+
+#- Lucia Fillippo
+#- Miguel Angel Victoria Simbaqueva 
+#- Irina Andrea Vélez López  
+#- Daniel Casas Bautista  
+
+# Initial configuration ---------------------------------------------------
+
+rm(list = ls())
+
+path_sript <- rstudioapi::getActiveDocumentContext()$path
+path_folder <- dirname(path_sript)
+setwd(path_folder)
