609578cd47631b7a723e5f9d442b669582005275
MiguelVictoria <m.victorias@uniandes.edu.co>
2023-07-29 23:43:22 -0500

    Update

---
+++ scripts/4. Descriptive Analysis.R
@@ -0,0 +1,85 @@
+############################################################
+#         Problem Set 3. Predicting Poverty
+#           Big data and Machine Learning
+#             Universidad de los Andes
+##########################################################
+
+#Authors:
+
+#- Lucia Fillippo
+#- Miguel Angel Victoria Simbaqueva 
+#- Irina Andrea Vélez López  
+#- Daniel Casas Bautista  
+
+# Initial configuration ---------------------------------------------------
+
+rm(list = ls())
+
+path_sript <- rstudioapi::getActiveDocumentContext()$path
+path_folder <- dirname(path_sript)
+setwd(path_folder)
+
+# Libraries -----
+if(!require(pacman)) install.packages("pacman")
+require(pacman)
+
+p_load(tidyverse,rio,
+       sf,
+       leaflet,
+       tmaptools,
+       osmdata,
+       nngeo,
+       magrittr,
+       rgeos,
+       rio,
+       rstudioapi,
+       stargazer,
+       glmnet)
+
+# Importing databases
+
+i_test_hogares <- readRDS("../stores/i_test_hogares.rds")
+i_train_hogares <- readRDS("../stores/i_train_hogares.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
